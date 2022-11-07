0d8bc5caaab8706e92dc2d576dbefb5744be8eab
abusayeed4bd <abusayeed4bd@gmail.com>
2022-11-07 19:16:27 +0600

    project created

---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="card w-96 bg-base-100 rounded-none p-0 shadow-xl image-full">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <div className="card-body p-2 ">
+
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
